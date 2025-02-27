e747262e1729ead75c1e6de3ed7e39c904b5bcdd
sgkochar <sgkochar@gmail.com>
2025-02-27 14:59:44 +0200

    create Footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -554,59 +554,59 @@ const Home = () => {
       <div className="home-features d-f">
         <div className="features-wr d-f">
           <div className="features-caption d-f align-center">
-            <div><img src="/images/shape.png" alt="shape" /></div>
+            <div><img src="/images/sun.png" alt="sun" /></div>
             <div>Tank Top</div>
           </div>
           <div className="features-caption d-f align-center">
-            <div><img src="/images/shape.png" alt="shape" /></div>
+            <div><img src="/images/sun.png" alt="sun" /></div>
             <div>T-Shirt</div>
           </div>
           <div className="features-caption d-f align-center">
-            <div><img src="/images/shape.png" alt="shape" /></div>
+            <div><img src="/images/sun.png" alt="sun" /></div>
             <div>Long-Sleeve T-Shir</div>
           </div>
           <div className="features-caption d-f align-center">
-            <div><img src="/images/shape.png" alt="shape" /></div>
+            <div><img src="/images/sun.png" alt="sun" /></div>
             <div>Raglan Sleeve Shirt</div>
           </div>
           <div className="features-caption d-f align-center">
-            <div><img src="/images/shape.png" alt="shape" /></div>
+            <div><img src="/images/sun.png" alt="sun" /></div>
             <div>Crop Top</div>
           </div>
           <div className="features-caption d-f align-center">
-            <div><img src="/images/shape.png" alt="shape" /></div>
+            <div><img src="/images/sun.png" alt="sun" /></div>
             <div>V-Neck Shirt</div>
           </div>
           <div className="features-caption d-f align-center">
-            <div><img src="/images/shape.png" alt="shape" /></div>
+            <div><img src="/images/sun.png" alt="sun" /></div>
             <div>Muscle Shirt</div>
           </div>
           <div className="features-caption d-f align-center">
-            <div><img src="/images/shape.png" alt="shape" /></div>
+            <div><img src="/images/sun.png" alt="sun" /></div>
             <div>Tank Top</div>
           </div>
           <div className="features-caption d-f align-center">
-            <div><img src="/images/shape.png" alt="shape" /></div>
+            <div><img src="/images/sun.png" alt="sun" /></div>
             <div>T-Shirt</div>
           </div>
           <div className="features-caption d-f align-center">
-            <div><img src="/images/shape.png" alt="shape" /></div>
+            <div><img src="/images/sun.png" alt="sun" /></div>
             <div>Long-Sleeve T-Shir</div>
           </div>
           <div className="features-caption d-f align-center">
-            <div><img src="/images/shape.png" alt="shape" /></div>
+            <div><img src="/images/sun.png" alt="sun" /></div>
             <div>Raglan Sleeve Shirt</div>
           </div>
           <div className="features-caption d-f align-center">
-            <div><img src="/images/shape.png" alt="shape" /></div>
+            <div><img src="/images/sun.png" alt="sun" /></div>
             <div>Crop Top</div>
           </div>
           <div className="features-caption d-f align-center">
-            <div><img src="/images/shape.png" alt="shape" /></div>
+            <div><img src="/images/sun.png" alt="sun" /></div>
             <div>V-Neck Shirt</div>
           </div>
           <div className="features-caption d-f align-center">
-            <div><img src="/images/shape.png" alt="shape" /></div>
+            <div><img src="/images/sun.png" alt="sun" /></div>
             <div>Muscle Shirt</div>
           </div>
         </div>
